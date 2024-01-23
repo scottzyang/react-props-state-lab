5d518adc82d19cd9ae8028212524f97394113ff0
scottzyang <scottzyang@gmail.com>
2024-01-22 19:23:00 -0600

    Finish Lab 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import Counter from './Counter'
 
 function App() {
+  const [count, setCount] = useState([1,4,3])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {count.map((value, index) => {
+        return (
+          <Counter
+            label={`counter ${index}`}
+            value={value}
+
+            increment={() => {
+              const newCount = [...count]
+              newCount[index] += 1
+              setCount(newCount)
+            }}
+
+            decrement={() => {
+              const newCount = [...count]
+              newCount[index] -= 1
+              setCount(newCount)
+            }}
+
+          />)
+      })}
     </div>
   );
 }
